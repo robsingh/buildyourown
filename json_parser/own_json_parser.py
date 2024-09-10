499ac28763d79f5bd7004699f6c9865018df622d
robsingh <robie10895@gmail.com>
2024-09-10 11:54:05 -0400

    updating file structure

---
+++ json_parser/own_json_parser.py
@@ -0,0 +1,17 @@
+'''
+This challenge is to build your own JSON parser.
+
+Building a JSON parser is an easy way to learn about parsing techniques which are useful for everything from parsing simple data formats through to building a fully featured compiler for a programming language.
+
+Parsing is often broken up into two stages: lexical analysis and syntactic analysis. 
+Lexical analysis is the process of dividing a sequence of characters into meaningful chunks, called tokens. 
+Syntactic analysis (which is also sometimes referred to as parsing) is the process of analysing the list of tokens to match it to a formal grammar.
+
+JSON - JavaScript Object Notation - a lightweight data-interchange format, which is widely used for transmitting data over the internet.
+'''
+
+'''
+Step 1:
+
+
+'''
