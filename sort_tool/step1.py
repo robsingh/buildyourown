25d2d99405f63d3cd653620f91a0b72c32e690ef
Levin Shah <levinshah@Levin-MBP.lan>
2025-06-26 17:50:26 -0700

    new challenge

---
+++ sort_tool/step1.py
@@ -0,0 +1,35 @@
+'''
+we want to be able to run sort and have it open a file and output the lines in the file sorted lexicographically.
+
+sort words.txt | uniq | head -n5
+A
+ACTUAL
+AGREE
+AGREEMENT
+AND
+'''
+
+'''
+
+notes:
+1. let's begin with opening the file. - done
+2. think how to sort the lines - thinking
+'''
+
+FILE_PATH = '/Users/levinshah/pypy/buildyourown/sort_tool/test.txt'
+def read_file():
+    try:
+        with open(FILE_PATH, 'r') as file:
+            content = file.read().strip()
+            if not content:
+                print(f"The file is empty. Please verify the file before proceeding.")
+                sys.exit(1)
+            print(content)
+    
+    except FileNotFoundError:
+        print(f"File not found.")
+
+
+
+
+read_file()
